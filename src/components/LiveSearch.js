c53b3d31ef7837006bb83671d46cf44fc23bb516
Nate Garrett <nate.garrett1992@gmail.com>
2017-11-07 14:18:14 -0600

    finished drill

--- src/components/LiveSearch.js
+++ src/components/LiveSearch.js
@@ -1,13 +1,28 @@
 import React from 'react';
-
 import SearchForm from './SearchForm';
 import CharacterList from './CharacterList';
 
-export default function LiveSearch(props) {
-    return (
-        <div className="live-search">
-            <SearchForm />
-            <CharacterList characters={props.characters} />
-        </div>
-    );
+export default class LiveSearch extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          searchTerm: ''
+      }
+  }
+  
+  render() {
+    
+      const characters = this.props.characters.filter(character =>
+          character.name.toLowerCase().includes(
+              this.state.searchTerm.toLowerCase()
+          )
+      );
+      return (
+          <div className="live-search">
+              <SearchForm onChange={searchTerm => this.setState({searchTerm})} />
+              <CharacterList characters={characters} />
+          </div>
+      );
+  }
 }
+
